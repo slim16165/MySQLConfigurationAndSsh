85ab1cb60ef9219a0e9ff6b744d61d494b65972f
Gianluigi Salvi <g.salvi@lottomatica.com>
2023-05-31 00:04:09 +0200

    Implementato il caricamento automatico dell'ultimo sito in caso di connessione automatica

--- Config/WebsiteAppConfigBase.cs
+++ Config/WebsiteAppConfigBase.cs
@@ -1,11 +1,25 @@
 ﻿using MySQLConfigurationAndSsh.Config.Data;
 
-namespace MySQLConfigurationAndSsh.Config
+namespace MySQLConfigurationAndSsh.Config;
+
+public class WebsiteAppConfigBase
 {
-    public class WebsiteAppConfigBase
+    /// <summary>
+    /// Imposta quale sito debba essere autoload
+    /// </summary>
+    private static string? AutoloadSite
     {
-        public Data.MySqlConnectionAppConfig MySql { get; set; }
-        public SshCredentials SshCredentials { get; set; }
-        public string? ShortSiteName { get; set; }
+        get => SettingsSaver.LoadFromConfigFile("AutoloadSite");
+        set => SettingsSaver.SaveOnConfigFile("AutoloadSite", value);
+    }
+
+    public Data.MySqlConnectionAppConfig MySql { get; set; }
+    public SshCredentials SshCredentials { get; set; }
+    public string? ShortSiteName { get; set; }
+
+    public bool IsAutoload
+    {
+        get => ShortSiteName == WebsiteAppConfigBase.AutoloadSite;
+        set => WebsiteAppConfigBase.AutoloadSite = ShortSiteName;
     }
 }
