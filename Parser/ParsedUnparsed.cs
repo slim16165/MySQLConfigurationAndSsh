2bd92a6482666095d5d9128a9ff96a190fe08265
Gianluigi Salvi <g.salvi@lottomatica.com>
2023-12-03 23:50:05 +0100

    Improved ParsedUnparsed

--- Parser/ParsedUnparsed.cs
+++ Parser/ParsedUnparsed.cs
@@ -1,72 +1,68 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Globalization;
 
-namespace MySQLConfigurationAndSsh.Parser
+namespace MySQLConfigurationAndSsh.Parser;
+
+public class ParsedUnparsed<T>
 {
-    public class ParsedUnparsed<T>
+    public string ColumnName { get; }
+    public string Unparsed { get; }
+    public T Parsed { get; set; }
+    public bool IsParsed { get; }
+
+
+    public ParsedUnparsed(string columnName, string unparsed, Func<string, T> conversionMethod = null)
     {
-        public string Unparsed { get; }
-        public T Parsed { get; set; }
-
-        public ParsedUnparsed(string unparsed, Func<string, T> conversionMethod = null)
+        ColumnName = columnName;
+        Unparsed = unparsed;
+        try
         {
-            Unparsed = unparsed;
-            try
-            {
-                if (conversionMethod != null)
-                    Parsed = conversionMethod(unparsed);
-                else
-                    Parsed = ParseStringToValue(unparsed);
-            }
-            catch (Exception e)
-            {
-                Parsed = default;
-            }
+            Parsed = conversionMethod != null ? conversionMethod(unparsed) : ParseStringToValue<T>(unparsed);
+        }
+        catch (Exception e)
+        {
+            Parsed = default;
+            IsParsed = false;
+        }
+    }
+
+
+    protected static T ParseStringToValue<T>(string cellValue)
+    {
+        //https://github.com/deniszykov/TypeConversion
+        //https://github.com/t-bruning/UniversalTypeConverter
+        if (typeof(T) == typeof(DateTime))
+            return (T)(object)ParseDate(cellValue);
+
+        var converter = TypeDescriptor.GetConverter(typeof(T));
+        if (converter != null)
+        {
+            return (T)converter.ConvertFromString(cellValue);
+        }
+        return default;
+    }
+
+
+    private static T ParseDate(string cellValue)
+    {
+        CultureInfo enUS = new CultureInfo("en-US");
+        if (DateTime.TryParseExact(cellValue, "yyyy-MM-dd HH.mm.ss", enUS, DateTimeStyles.None,
+                out DateTime result))
+            return (T)(object)result;
+        else throw new Exception(cellValue + "non è una data valida");
+    }
+
+    public override string ToString()
+    {
+        if (typeof(T) != typeof(string))
+        {
+            if (Parsed.ToString() != Unparsed && Unparsed != null)
+                return $"{Parsed} ({Unparsed})";
+            else
+                return Parsed.ToString();
         }
 
-
-        protected static T ParseStringToValue(string cellValue)
-        {
-            if (typeof(T) == typeof(DateTime))
-                return ParseDate(cellValue);
-
-            if (typeof(T) == typeof(decimal))
-                return (T)(object)Convert.ToDecimal(cellValue);
-
-            if (typeof(T) == typeof(double))
-                return (T)(object)Convert.ToDouble(cellValue);
-
-            if (typeof(T) == typeof(int))
-                return (T)(object)Convert.ToInt32(cellValue);
-
-            if (typeof(T) == typeof(int?))
-                return (T)(object)Convert.ToInt32(cellValue);
-
-            return (T)(object)cellValue;
-        }
-
-        private static T ParseDate(string cellValue)
-        {
-            CultureInfo enUS = new CultureInfo("en-US");
-            if (DateTime.TryParseExact(cellValue, "yyyy-MM-dd HH.mm.ss", enUS, DateTimeStyles.None,
-                out DateTime prova1))
-                return (T)(object)prova1;
-            else throw new Exception(cellValue + "non è una data valida");
-        }
-
-        public override string ToString()
-        {
-            if (typeof(T) != typeof(string))
-            {
-                if (Parsed.ToString() != Unparsed && Unparsed != null)
-                    return $"{Parsed} ({Unparsed})";
-                else
-                    return Parsed.ToString();
-            }
-
-            return Unparsed;
-        }
-
-       
+        return Unparsed;
     }
 }
